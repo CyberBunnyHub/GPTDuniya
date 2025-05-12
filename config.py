55b27e529d79448e0d529b33f5df83b6a86d0168
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-12 15:58:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+
+MONGO_URI = "mongodb+srv://CyberBunny:Bunny2008@cyberbunny.5yyorwj.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "CyberBunny"
+COLLECTION_NAME = "CBI"
+BOT_TOKEN = "7845318227:AAGfr0cikK92HY-DhIGMVTD3L0VeaKktUp8"
+DB_CHANNEL = -1002511163521
+
+# UI Elements
+IMAGE_URLS = [
+    "https://telegra.ph/file/00b5e7f5bcf7ccf0a67c4.jpg",
+    "https://telegra.ph/file/6883a29be71655fd43c8c.jpg",
+    "https://telegra.ph/file/84c66d3ad8e35cc4df973.jpg"
+]
+CAPTIONS = [
+    "Hey there! I'm your friendly Auto Filter bot.",
+    "Hello! Ready to search your favorite files.",
+    "Hi! Send me a movie name to get started."
+]
+
+# Support Links
+UPDATE_CHANNEL = "https://t.me/YourUpdateChannel"
+SUPPORT_GROUP = "https://t.me/VirabGroup"
