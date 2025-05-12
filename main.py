6d29134116117904251f044136efe6778a27fddc
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-12 12:14:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+app = Client("autofilter_bot")
+
+@app.on_message(filters.command("start"))
+async def start(_, message: Message):
+    await message.reply_photo(
+        photo="https://placehold.co/600x400",
+        caption="I can help you auto filter files in groups!
+Just add me and send a message.",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Add Me To Group", url="https://t.me/YourBot?startgroup=true")],
+             [InlineKeyboardButton("About", callback_data="about"),
+              InlineKeyboardButton("Help", callback_data="help")]]
+        )
+    )
+
+app.run()
