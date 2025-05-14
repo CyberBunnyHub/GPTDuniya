fa8f31ba75f11c6f7bc7ba09b907b1bace48a494
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-14 20:08:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,6 +92,8 @@ async def start_cmd(client, message: Message):
         [InlineKeyboardButton("Hᴇʟᴘ", callback_data="help"), InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about")],
         [InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url=UPDATE_CHANNEL), InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP)]
     ])
+print(caption)
+await message.reply_photo(image, caption=caption, reply_markup=keyboard)
 
 # Bot added to group
 @app.on_message(filters.new_chat_members)
