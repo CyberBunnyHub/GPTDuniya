144e78870a6480f06c7e44891b4759dca452a573
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-14 14:29:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ API_HASH = "0a33bc287078d4dace12aaecc8e73545"
 MONGO_URI = "mongodb+srv://CyberBunny:Bunny2008@cyberbunny.5yyorwj.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "CyberBunny"
 COLLECTION_NAME = "CBI"
-BOT_TOKEN = "7850386310:AAEeCeBvPdkwThjHSOjHecEhWfzI4DQ8JsM"
+BOT_TOKEN = "7845318227:AAGfr0cikK92HY-DhIGMVTD3L0VeaKktUp8"
 DB_CHANNEL = -1002511163521
 
 # UI Elements
 IMAGE_URLS = [
-    "https://i.ibb.co/dJw7sbCh/5a5b38ed84fb.jpg"
+    "https://envs.sh/oeA.jpg"
 ]
 
 CAPTIONS = [
-    "Hey there! I'm your friendly Auto Filter bot.",
-    "Hello! Ready to search your favorite files.",
-    "Hi! Send me a movie name to get started."
+    "<blockquote>ʜᴇʟʟᴏ {}👋,</blockquote>
+ 
+I'ᴍ Lᴀᴛᴇꜱᴛ Aᴅᴠᴀɴᴄᴇᴅ & Pᴏᴡᴇʀꜰᴜʟ Aᴜᴛᴏ Fɪʟᴛᴇʀ Bᴏᴛ. Yᴏᴜ Cᴀɴ Uꜱᴇ Mᴇ Tᴏ Gᴇᴛ Mᴏᴠɪᴇs [ Jᴜsᴛ Sᴇɴᴅ Mᴇ Mᴏᴠɪᴇ Nᴀᴍᴇ ] Oʀ Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ & Mᴀɢɪᴄ Hᴀᴘᴘᴇɴs!."
 ]
 
 # Support Links
-UPDATE_CHANNEL = "https://t.me/YourUpdateChannel"
+UPDATE_CHANNEL = "https://t.me/+giwNAm14E38xYjI9"
 SUPPORT_GROUP = "https://t.me/VirabGroup"
