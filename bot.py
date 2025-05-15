afcb62deb6d6bda12ceabbd52104aac6b2b5ad4c
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-15 14:48:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def welcome_new_members(client, message: Message):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url=UPDATE_CHANNEL), InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP)]
     ])
-            await message.reply("TʜᴀɴᴋYᴏᴜ! Fᴏʀ Aᴅᴅɪɴɢ Mᴇh Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ <a href="{group_link}">{chat_title}</a> , Lᴇᴛs Sᴛᴀʀᴛ Tʜᴇ Gᴀᴍᴇ...😂", reply_markup=keyboard)
+            await message.reply(f'TʜᴀɴᴋYᴏᴜ! Fᴏʀ Aᴅᴅɪɴɢ Mᴇh Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ <a href="{group_link}">{chat_title}</a> , Lᴇᴛs Sᴛᴀʀᴛ Tʜᴇ Gᴀᴍᴇ...😂', reply_markup=keyboard)
 
 
 # Save files from DB_CHANNEL
