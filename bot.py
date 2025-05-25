164525a806888796ed5ae5c80ed00ccc38540f78
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-25 15:12:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -185,10 +185,10 @@ async def handle_callbacks(client, query: CallbackQuery):
 
         if not results:
             return await query.answer("No files found.", show_alert=True)
-
-        markup = generate_pagination_buttons(results, (await client.get_me()).username, page, 5, prefix, query_text, query.from_user.id)
-        await query.message.edit_reply_markup(markup)
-        return await query.answer()
+            
+            markup = await generate_pagination_buttons(results, (await client.get_me()).username, page, 5, prefix, query_text, query.from_user.id)
+            await query.message.edit_reply_markup(markup)
+            return await query.answer()
 
     elif data.startswith("deletefile:"):
         file_id = data.split(":")[1]
